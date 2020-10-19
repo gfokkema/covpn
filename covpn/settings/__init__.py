9ed04dce2fef9c7e09554eb7e45b2e1f15c61dab
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2020-10-19 21:32:21 +0200

    Split settings into defaults and site_settings outside of vcs.

---
+++ covpn/settings/__init__.py
@@ -0,0 +1,2 @@
+from .defaults import *
+from .site_settings import *
--- covpn/settings/defaults.py
+++ covpn/settings/defaults.py
@@ -13,21 +13,19 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8kle8n-^9ta*-bu9n%%$5472y)9m7@q481&!6jfd#m!*w##sae'
+SECRET_KEY = None  # fill me in
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'vps0441.redkiwi.cloud'
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -81,10 +79,8 @@ WSGI_APPLICATION = 'covpn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'covpn',
-        'USER': 'covpn',
-        'PASSWORD': 'queih8eeT#u#xe',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
